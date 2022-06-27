116179bfbb4ab3f40c3daee61adc7f1bb1bdc0e3
ronshen0404 <ronshen0404@gmail.com>
2022-06-27 18:38:11 +0800

    add model details and some template files

--- ebookstore/books/models.py
+++ ebookstore/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from comment_rating.models import CommentRating
+from users.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -26,17 +27,19 @@ class Book(models.Model):
   image = models.ImageField(upload_to=None)
   pdf = models.FileField(upload_to=None)
   category = models.ForeignKey(Category, on_delete=models.PROTECT)
-  author = models.ManyToManyField(Author)
-  publisher = models.ForeignKey(Publisher)
+  author = models.ManyToManyField(Author, on_delete=models.PROTECT)
+  publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT)
   
   
-class UserBook(models.Model):
-    class Meta:
-        unique_together = ['user_id', 'book_id']
+class UserBook(models.Model): 
+  #since Django doesn't support composite primary keys,
+  #unique_together need to be used ot act as composite primary keys
+  class Meta:
+      unique_together = ['user_id', 'book_id']
 
-    user_id = models.IntegerField(primary_key=True)
-    book_id = models.IntegerField()
-    comment_rating = models.OneToOneField(CommentRating)
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+  comment_rating = models.OneToOneField(CommentRating, on_delete=models.CASCADE)
   
   
   
